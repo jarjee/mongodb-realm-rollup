c567ee05c717dd7c88c24a70cb2e432b25d2f807
Nathan Smyth <nathan.smyth@mongodb.com>
2021-04-14 16:37:45 +1000

    feat: Now parses JSDocs for functions settings

--- dist/index.js
+++ dist/index.js
@@ -4,6 +4,7 @@ var fs = require('fs');
 var path = require('path');
 var walkdir = require('walkdir');
 var picomatch = require('picomatch');
+var commentParser = require('comment-parser');
 
 function _interopDefaultLegacy (e) { return e && typeof e === 'object' && 'default' in e ? e : { 'default': e }; }
 
@@ -12,6 +13,7 @@ var path__default = /*#__PURE__*/_interopDefaultLegacy(path);
 var walkdir__default = /*#__PURE__*/_interopDefaultLegacy(walkdir);
 var picomatch__default = /*#__PURE__*/_interopDefaultLegacy(picomatch);
 
+const PLUGIN_NAME = 'mongodb-realm';
 function isEmpty(x) {
     return x === undefined || x === '' || (Array.isArray(x) && x.length === 0);
 }
@@ -21,13 +23,63 @@ function multiMap(v, fn) {
     }
     return fn(v);
 }
+function parseFunctionJSDoc(jsDoc) {
+    const specs = jsDoc.flatMap(block => block.tags);
+    const result = specs.reduce((acc, spec) => {
+        switch (spec.tag.toLowerCase()) {
+            case 'public':
+                acc.public = true;
+                break;
+            case 'private':
+                acc.private = true;
+                break;
+            case 'system':
+                acc.system = true;
+                break;
+            case 'user':
+                acc.userId = spec.type;
+                break;
+            case 'logarguments':
+                acc.disableArgumentLogging = ['', 'true'].includes(spec.type)
+                    ? undefined
+                    : true;
+                break;
+            // Deliberately do nothing
+        }
+        return acc;
+    }, { disableArgumentLogging: true });
+    return result;
+}
+function validateFunctionSettings(settings, id) {
+    const result = Object.assign({}, settings);
+    if (settings.private && settings.public) {
+        console.log(`[${PLUGIN_NAME}]: Cannot have both private & public; setting as private in ${id}.`);
+        delete result.public;
+    }
+    // TODO: Also handle script case when implemented
+    if (settings.userId && settings.system) {
+        console.log(`[${PLUGIN_NAME}]: Cannot have multiple authentications, setting as 'user' in ${id}.`);
+        delete result.system;
+    }
+    return result;
+}
+function functionSettingsToRealm(name, settings) {
+    return {
+        name,
+        private: settings.private || settings.public === undefined ? true : false,
+        run_as_system: settings.system,
+        run_as_user_id: settings.userId,
+        disable_arg_logs: settings.disableArgumentLogging,
+    };
+}
+const functionSettingsCache = new Map();
 function realm(pluginOptions) {
     return {
-        name: 'mongodb-realm',
+        name: PLUGIN_NAME,
         options: options => {
             const inputEntries = [];
             if (!isEmpty(options.input)) {
-                console.warn('[mongodb-realm]: You cannot specify inputs since we mangle them for you.');
+                console.error(`[${PLUGIN_NAME}]: You cannot specify inputs since we add them for you.`);
                 return null;
             }
             const rootAbsolute = path__default['default'].resolve(pluginOptions.rootPath);
@@ -50,7 +102,25 @@ function realm(pluginOptions) {
         },
         // Going to apply a very simple transform.
         // Could this be better? Definitely.
-        transform: code => {
+        transform: (code, id) => {
+            // Test if the code has to be transformed
+            if (!id.match(path__default['default'].join(pluginOptions.rootPath, 'functions'))) {
+                return null;
+            }
+            if (!/export default/.test(code)) {
+                console.warn(`[${PLUGIN_NAME}]: We expect 'export default' in ${id}`);
+                return null;
+            }
+            // Dodgy comment parsing
+            const jsDocMatch = /(\/\*\*.+\*\/).*export default/gms.exec(code);
+            // If we find a JSDoc above the default export
+            // Note: We check for 2 as the matches are:
+            // - The complete regex
+            // - The comment group we're interested in
+            if ((jsDocMatch === null || jsDocMatch === void 0 ? void 0 : jsDocMatch.length) === 2) {
+                const functionSettings = validateFunctionSettings(parseFunctionJSDoc(commentParser.parse(jsDocMatch[1])), id);
+                functionSettingsCache.set(id, functionSettings);
+            }
             return code.replace('export default', 'exports =');
         },
         // MongoDB Realm has a seperate dynamic import system
@@ -61,12 +131,19 @@ function realm(pluginOptions) {
                 if (file.type === 'chunk') {
                     const folder = path__default['default'].join(options.dir || 'dir', file.fileName.replace('/source.js', ''));
                     // Check if the config.json exists.
-                    // If it does, leave it alone.
+                    // If it does, merge with what we've parsed
                     // If it doesn't, create the config file
                     const configFile = path__default['default'].join(folder, 'config.json');
-                    if (!fs__default['default'].existsSync(configFile)) {
-                        const config = { name: path__default['default'].basename(folder), private: true };
-                        fs__default['default'].writeFileSync(configFile, JSON.stringify(config, null, 4));
+                    const rawOriginalConfig = fs__default['default'].existsSync(configFile) && fs__default['default'].readFileSync(configFile, 'utf8');
+                    // If it's a function, use the default function config
+                    if (/\/functions\//.test(configFile)) {
+                        const name = path__default['default'].basename(folder);
+                        const parsedConfig = functionSettingsCache.get(file.facadeModuleId || '');
+                        const functionConfig = parsedConfig
+                            ? functionSettingsToRealm(name, parsedConfig)
+                            : { name, private: true, disable_arg_logs: true };
+                        fs__default['default'].writeFileSync(configFile, JSON.stringify(rawOriginalConfig
+                            ? Object.assign({ id: JSON.parse(rawOriginalConfig).id }, functionConfig) : functionConfig, null, 4));
                     }
                 }
             }
--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,13 @@ import {Plugin} from 'rollup';
 
 import fs from 'fs';
 import path from 'path';
+
 import walkdir from 'walkdir';
 import picomatch from 'picomatch';
+import {parse} from 'comment-parser';
+import type {Block} from 'comment-parser';
+
+const PLUGIN_NAME = 'mongodb-realm';
 
 type RealmOptions = {
   rootPath: string;
@@ -21,19 +26,102 @@ function multiMap<T, V>(v: T | T[], fn: (x: T) => V) {
   return fn(v);
 }
 
-export default function realm(pluginOptions: RealmOptions): Plugin {
-  const picoMatchers = {
-    function: undefined,
-  };
+type FunctionSettings = {
+  private?: true;
+  public?: true;
+  system?: true;
+  userId?: string;
+  disableArgumentLogging?: true;
+};
 
+type RealmFunctionSettings = {
+  name: string;
+  private: boolean;
+  run_as_system?: true;
+  run_as_user_id?: string;
+  disable_arg_logs?: true;
+};
+
+function parseFunctionJSDoc(jsDoc: Block[]): FunctionSettings {
+  const specs = jsDoc.flatMap(block => block.tags);
+
+  const result = specs.reduce(
+    (acc, spec) => {
+      switch (spec.tag.toLowerCase()) {
+        case 'public':
+          acc.public = true;
+          break;
+        case 'private':
+          acc.private = true;
+          break;
+        case 'system':
+          acc.system = true;
+          break;
+        case 'user':
+          acc.userId = spec.type;
+          break;
+        case 'logarguments':
+          acc.disableArgumentLogging = ['', 'true'].includes(spec.type)
+            ? undefined
+            : true;
+          break;
+        default:
+        // Deliberately do nothing
+      }
+      return acc;
+    },
+    {disableArgumentLogging: true} as FunctionSettings
+  );
+
+  return result;
+}
+
+function validateFunctionSettings(settings: FunctionSettings, id: string) {
+  const result = {...settings};
+  if (settings.private && settings.public) {
+    console.log(
+      `[${PLUGIN_NAME}]: Cannot have both private & public; setting as private in ${id}.`
+    );
+
+    delete result.public;
+  }
+
+  // TODO: Also handle script case when implemented
+  if (settings.userId && settings.system) {
+    console.log(
+      `[${PLUGIN_NAME}]: Cannot have multiple authentications, setting as 'user' in ${id}.`
+    );
+
+    delete result.system;
+  }
+
+  return result;
+}
+
+function functionSettingsToRealm(
+  name: string,
+  settings: FunctionSettings
+): RealmFunctionSettings {
   return {
-    name: 'mongodb-realm',
+    name,
+    private: settings.private || settings.public === undefined ? true : false,
+    run_as_system: settings.system,
+    run_as_user_id: settings.userId,
+    disable_arg_logs: settings.disableArgumentLogging,
+  };
+}
+
+const functionSettingsCache = new Map<string, FunctionSettings>();
+
+export default function realm(pluginOptions: RealmOptions): Plugin {
+  return {
+    name: PLUGIN_NAME,
     options: options => {
       const inputEntries: [string, string][] = [];
 
       if (!isEmpty(options.input)) {
-        console.warn(
-          '[mongodb-realm]: You cannot specify inputs since we mangle them for you.'
+        console.error(
+          `[${PLUGIN_NAME}]: You cannot specify inputs since we add them for you.`
         );
         return null;
       }
@@ -75,7 +163,32 @@ export default function realm(pluginOptions: RealmOptions): Plugin {
     },
     // Going to apply a very simple transform.
     // Could this be better? Definitely.
-    transform: code => {
+    transform: (code, id) => {
+      // Test if the code has to be transformed
+      if (!id.match(path.join(pluginOptions.rootPath, 'functions'))) {
+        return null;
+      }
+
+      if (!/export default/.test(code)) {
+        console.warn(`[${PLUGIN_NAME}]: We expect 'export default' in ${id}`);
+        return null;
+      }
+
+      // Dodgy comment parsing
+      const jsDocMatch = /(\/\*\*.+\*\/).*export default/gms.exec(code);
+
+      // If we find a JSDoc above the default export
+      // Note: We check for 2 as the matches are:
+      // - The complete regex
+      // - The comment group we're interested in
+      if (jsDocMatch?.length === 2) {
+        const functionSettings = validateFunctionSettings(
+          parseFunctionJSDoc(parse(jsDocMatch[1])),
+          id
+        );
+        functionSettingsCache.set(id, functionSettings);
+      }
+
       return code.replace('export default', 'exports =');
     },
     // MongoDB Realm has a seperate dynamic import system
@@ -91,12 +204,34 @@ export default function realm(pluginOptions: RealmOptions): Plugin {
           );
 
           // Check if the config.json exists.
-          // If it does, leave it alone.
+          // If it does, merge with what we've parsed
           // If it doesn't, create the config file
           const configFile = path.join(folder, 'config.json');
-          if (!fs.existsSync(configFile)) {
-            const config = {name: path.basename(folder), private: true};
-            fs.writeFileSync(configFile, JSON.stringify(config, null, 4));
+
+          const rawOriginalConfig =
+            fs.existsSync(configFile) && fs.readFileSync(configFile, 'utf8');
+
+          // If it's a function, use the default function config
+          if (/\/functions\//.test(configFile)) {
+            const name = path.basename(folder);
+            const parsedConfig = functionSettingsCache.get(
+              file.facadeModuleId || ''
+            );
+
+            const functionConfig = parsedConfig
+              ? functionSettingsToRealm(name, parsedConfig)
+              : {name, private: true, disable_arg_logs: true};
+
+            fs.writeFileSync(
+              configFile,
+              JSON.stringify(
+                rawOriginalConfig
+                  ? {id: JSON.parse(rawOriginalConfig).id, ...functionConfig}
+                  : functionConfig,
+                null,
+                4
+              )
+            );
           }
         }
       }
